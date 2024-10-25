a0a653b7fd0475343aebe720b749976426ca8cf6
jainex17 <jainexp017@gmail.com>
2024-10-26 03:06:11 +0530

    client side dashboard

---
+++ frontend/app/client/page.tsx
@@ -0,0 +1,14 @@
+import { SideBar } from "@/components/client/SideBar";
+import React from "react";
+
+const Page = () => {
+  return <>
+    <SideBar />
+
+    <div className="sm:mx-72 mx-16 my-12">
+        dsauh
+    </div>
+  </>;
+};
+
+export default Page;

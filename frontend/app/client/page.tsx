ff71b1609e1581f9e61b2b4852e43f10b0a481d9
jainex17 <jainexp017@gmail.com>
2024-10-26 04:20:16 +0530

    Refactor project structure and add client dashboard components

--- frontend/app/client/page.tsx
+++ frontend/app/client/page.tsx
@@ -1,3 +1,7 @@
+
+import Cards from "@/components/client/Dashboard/Cards";
+import Chart from "@/components/client/Dashboard/Chart";
+import { Tables } from "@/components/client/Dashboard/Tables";
 import { SideBar } from "@/components/client/SideBar";
 import React from "react";
 
@@ -5,8 +9,18 @@ const Page = () => {
   return <>
     <SideBar />
 
-    <div className="sm:mx-72 mx-16 my-12">
-        dsauh
+    <div className="sm:ml-72 mx-16 my-12">
+        <Cards />
+
+        <div className="my-20 flex justify-between gap-5">
+          <div className="w-full">
+
+            <Tables />
+          </div>
+            <div className="w-1/3">
+              <Chart />
+            </div>
+        </div>
     </div>
   </>;
 };

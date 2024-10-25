6759f157adb7a56daf0a99224bbd5c9c9f544e79
Rahul Biradar <126542436+rahulbiradar06@users.noreply.github.com>
2024-10-26 03:31:02 +0530

    .

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,9 +1,18 @@
+<<<<<<< Updated upstream
+=======
+import { Dashboard } from "@/components/warehouse/Dashboard";
+>>>>>>> Stashed changes
 
 
 export default function Home() {
   return (
     <>
+<<<<<<< Updated upstream
   
+=======
+
+      <Dashboard />
+>>>>>>> Stashed changes
     </>
   );
 }

8b6e36aafc6c8f68030443760e25042e8eff537a
jainex17 <jainexp017@gmail.com>
2024-10-26 01:56:37 +0530

    Refactor project structure and remove unused files

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -1,5 +1,3 @@
-const {nextui} = require("@nextui-org/react");
-
 import type { Config } from "tailwindcss";
 
 const config: Config = {
@@ -7,7 +5,6 @@ const config: Config = {
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
@@ -18,6 +15,6 @@ const config: Config = {
     },
   }, 
   darkMode: "class",
-  plugins: [nextui()],
+  plugins: [],
 };
 export default config;
